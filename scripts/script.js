2a08a2d80c451802f41e8351d99e00ffe1b85c83
Aaron Huggins <huggins.aaron@outlook.com>
2021-11-07 18:57:36 -0800

    Reset and prompt user for grid size

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,49 @@
 //Get grid id from HTML, to create grid inside.
 const grid = document.querySelector('#grid');
 
+
 //Preset gridSize, to start. To be changed by user after.
 let gridSize = 8;
 
+
+//Create squares when page has loaded.
+document.addEventListener("DOMContentLoaded", function() {
+    createSquare(gridSize);
+  });
+  
+
+//Create reset button above the grid
+const resetButton = document.createElement('button');
+resetButton.innerHTML = 'Reset Grid';
+resetButton.addEventListener('click',function(){
+    getGridSize();
+}); //<-- When clicked, reset the grid and ask for new grid size
+grid.parentNode.insertBefore(resetButton,grid); //<-- Add button on top of grid
+
+
+//===================FUNCTIONS===================================//
+
 //Create gridSize * gridSize squares to fill a 960x960 grid.
 function createSquare(gridSize) {
+    //Create a grid of gridSize rows and columns.
+    grid.style.grid = `repeat(${gridSize}, 1fr)/repeat(${gridSize}, 1fr)`; 
     for (let i = 1; i <= gridSize*gridSize; i++){
         let newSquare = document.createElement('div'); 
         newSquare.classList.add('square');// <-- Adds the square class to each square so CSS rules apply
-
-        //Randomize background color
+        
+        //Randomize square color on creation
         let squareColor = randomColor();
         newSquare.style.backgroundColor = squareColor;
-
-        //Darken on mouse over
+        
+        //Randomize square color on mouse over
+        newSquare.addEventListener("mouseover", function(){
+            newSquare.style.backgroundColor = randomColor();
+        });
 
         grid.appendChild(newSquare); // <-- Adds created square to the grid
     }
 }
+
 
 //Create random color
 function randomColor() {
@@ -30,9 +55,23 @@ function randomColor() {
     return rgb;
 }
 
-//Create a grid of gridSize rows and columns.
-grid.style.grid = `repeat(${gridSize}, 1fr)/repeat(${gridSize}, 1fr)`; 
 
-//Create squares when page has loaded.
-document.ready(createSquare(gridSize))
+//Reset grid with new user specified grid size
+function getGridSize() {
+    let input = prompt("Specify Grid Size (Min:1 / Max:100)", "64");
+    while(input <= 0 || input > 100 || input == null) {
+        input = prompt("Specify Grid Size (Min:1 / Max:100)", "64");
+    }
+    removeSquares();
+    createSquare(input);
+}
+
+
+//Remove all squares on reset
+function removeSquares(){
+    const elements = document.getElementsByClassName('square');
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
 
